815784a860e1a95e06fc0915c893bfc7e0269ba8
Thomas Arrow <thomasarrow@gmail.com>
2016-11-11 16:06:48 +0000

    first implementation of swagger

--- dummyfactstore.js
+++ dummyfactstore.js
@@ -5,19 +5,40 @@ var dummyFactStore = function() {
 
 }
 
-var convertESDump2OutputForm = function(fulldump) {
+var convertESLine2OutputForm = function (line, date) {
+  if (line) {
+    var lineOut = line._source
+    lineOut.ingestionDate = date
+    return lineOut
+  }
+  return null
+}
+
+var convertESDump2OutputForm = function(fulldump, date) {
+  //console.log('converting ES to output JSON')
   var stringLines = _.split(fulldump,'\n')
-  var jsonLines = _.map(stringLines, function(foo) { JSON.parse(foo) })
-  console.log(jsonLines)
+  //console.dir(stringLines)
+  var jsonLines = _.map(stringLines, function(line) {
+    if (line) {
+      var jsonline = JSON.parse(line)
+      return convertESLine2OutputForm(jsonline, date)
+    }
+    else {
+      return null
+    }
+  })
+  //console.log('finished mapping')
+  //console.log(jsonLines)
   return new Promise(function(resolve, reject) {
     resolve(jsonLines)
   })
 }
 
-dummyFactStore.prototype.getByDate = function() {
+dummyFactStore.prototype.getByDate = function(date) {
   return new Promise(function(resolve, reject) {
+    //console.log('reading from file')
     fsp.readFile('sample.json', {encoding:'utf8'})
-    .then(convertESDump2OutputForm)
+    .then(line => { return convertESDump2OutputForm(line, date) })
     .then(resolve)
   });
 }
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express()
 var router = express.Router()
 var factstore = require('./dummyfactstore')
 var port = process.env.PORT || 8080;
+var bodyParser = require( 'body-parser' );
+var subpath = express();
+var swagger = require('swagger-node-express').createNew(subpath);
+
 
 factStore = new factstore()
 router.get('/date/:date', (req, res) => {
@@ -15,5 +19,14 @@ router.get('/date/:date', (req, res) => {
   })
 })
 
+
+app.use(bodyParser());
+app.use(express.static('dist'));
+//app.use("/v1", subpath);
 app.use('/api', router)
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+
 app.listen(port)
