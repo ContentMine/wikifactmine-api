0ddea9c9c7456b0ecd6d3846a4e97f3951123387
Thomas Arrow <thomasarrow@gmail.com>
2017-02-16 11:31:59 +0000

    convert dummy.js to standard-js

--- dummyfactstore.js
+++ dummyfactstore.js
@@ -1,7 +1,7 @@
 var fsp = require('fs-promise')
 var _ = require('lodash')
 
-var dummyFactStore = function() {
+var dummyFactStore = function () {
 
 }
 
@@ -14,33 +14,27 @@ var convertESLine2OutputForm = function (line, date) {
   return null
 }
 
-var convertESDump2OutputForm = function(fulldump, date) {
-  //console.log('converting ES to output JSON')
-  var stringLines = _.split(fulldump,'\n')
-  //console.dir(stringLines)
-  var jsonLines = _.map(stringLines, function(line) {
+var convertESDump2OutputForm = function (fulldump, date) {
+  var stringLines = _.split(fulldump, '\n')
+  var jsonLines = _.map(stringLines, function (line) {
     if (line) {
       var jsonline = JSON.parse(line)
       return convertESLine2OutputForm(jsonline, date)
-    }
-    else {
+    } else {
       return null
     }
   })
-  //console.log('finished mapping')
-  //console.log(jsonLines)
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     resolve(jsonLines)
   })
 }
 
-dummyFactStore.prototype.getByDate = function(date) {
-  return new Promise(function(resolve, reject) {
-    //console.log('reading from file')
-    fsp.readFile('sample.json', {encoding:'utf8'})
-    .then(function(line) { return convertESDump2OutputForm(line, date) })
+dummyFactStore.prototype.getByDate = function (date) {
+  return new Promise(function (resolve, reject) {
+    fsp.readFile('sample.json', {encoding: 'utf8'})
+    .then(function (line) { return convertESDump2OutputForm(line, date) })
     .then(resolve)
-  });
+  })
 }
 
 module.exports = dummyFactStore
